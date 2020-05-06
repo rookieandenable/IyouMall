9d002a8a98e3d210f7ca90bc3a0bf53d2a9b1103
sunjinhua466 <sunjinhua@51visa.com>
2020-05-07 01:25:36 +0800

    IyouMall

---
+++ README.md
@@ -0,0 +1,29 @@
+# demodemo2
+
+> this is a test about vue and mongoDB
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+#目录结构浅析
+#src
+前端源码
+#server
+服务器源码
+#启动服务
+node server/bin/www
---
+++ src/components/Modal.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':(mdShow || addrShow || toAdd)}">
+        <div class="md-modal-inner">
+          <div class="md-top">
+            <button class="md-close" @click="closeModal">Close</button>
+          </div>
+          <div class="md-content">
+            <!-- 修改地址 -->
+            <div class="confirm-tips" v-if="addrShow">
+              收件人姓名:<slot name="addrName"></slot>
+            </div>
+            <div class="confirm-tips" v-if="addrShow">
+              详细地址:<slot name="addrInfo"></slot>
+            </div>
+            <div class="confirm-tips" v-if="addrShow">
+              联系电话:<slot name="addrTel"></slot>
+            </div>
+            <!-- 添加地址 -->
+            <div class="confirm-tips" v-if="toAdd">
+              收件人姓名:<slot name="addrNameAdd"></slot>
+            </div>
+            <div class="confirm-tips" v-if="toAdd">
+              详细地址:<slot name="addrInfoAdd"></slot>
+            </div>
+            <div class="confirm-tips" v-if="toAdd">
+              联系电话:<slot name="addrTelAdd"></slot>
+            </div>
+            <!-- 提示信息 -->
+            <div class="confirm-tips" v-if="mdShow">
+              <slot name="message"></slot>
+            </div>
+            <div class="btn-wrap">
+              <slot name="btnGroup"></slot>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md-overlay" v-if="(mdShow || addrShow || toAdd)" @click="closeModal"></div>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+        props:["mdShow","addrShow","toAdd"],
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        methods:{
+          closeModal(){
+            this.$emit("close");
+          }
+        }
+    }
+</script>
